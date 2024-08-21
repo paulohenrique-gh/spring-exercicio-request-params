6422f6dd31f6c2cff972e7226ae03a48a9cdb865
Paulo Henrique <ph.1928@hotmail.com>
2024-08-21 20:44:15 -0300

    Commit inicial

---
+++ src/main/java/org/example/exercicioapirequestparams/ExercicioApiRequestParamsApplication.java
@@ -0,0 +1,13 @@
+package org.example.exercicioapirequestparams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioApiRequestParamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercicioApiRequestParamsApplication.class, args);
+    }
+
+}
