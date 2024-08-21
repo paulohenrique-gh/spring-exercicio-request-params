70b051f24cf3bd63d250b9ace8ac573a5b5dcd57
Paulo Henrique <ph.1928@hotmail.com>
2024-08-21 20:44:28 -0300

    Cria model Order

---
+++ src/main/java/org/example/exercicioapirequestparams/model/Order.java
@@ -0,0 +1,28 @@
+package org.example.exercicioapirequestparams.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderNumber;
+    private String customerName;
+    private String status;
+    private Integer totalAmount;
+    private LocalDateTime orderDate;
+}
