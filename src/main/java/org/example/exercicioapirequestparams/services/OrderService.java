970c63fb3c320633bf2ef1098cc6e9da8bcb093d
Paulo Henrique <ph.1928@hotmail.com>
2024-08-21 21:32:52 -0300

    Cria OrderService

---
+++ src/main/java/org/example/exercicioapirequestparams/services/OrderService.java
@@ -0,0 +1,35 @@
+package org.example.exercicioapirequestparams.services;
+
+import lombok.RequiredArgsConstructor;
+import org.example.exercicioapirequestparams.model.Order;
+import org.example.exercicioapirequestparams.repositories.OrderRepository;
+import org.example.exercicioapirequestparams.services.specifications.OrderSpecification;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public List<Order> getOrders(Long orderNumber,
+            String customerName,
+            String status,
+            Integer minAmount,
+            Integer maxAmount,
+            LocalDate startDate,
+            LocalDate endDate) {
+        Specification<Order> spec = Specification
+                .where(OrderSpecification.hasOrderNumber(orderNumber))
+                .and(OrderSpecification.hasCustomerName(customerName))
+                .and(OrderSpecification.hasStatus(status))
+                .and(OrderSpecification.hasAmountInRange(minAmount, maxAmount))
+                .and(OrderSpecification.hasDateRange(startDate, endDate));
+
+        return orderRepository.findAll(spec);
+    }
+}
